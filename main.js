eaa2124b586bfd747acae2291d3a07d83275a514
const_k11 <const_k11@mail.ru>
2023-03-13 12:32:51 +0300

    fetch fan id news

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 import 'https://code.jquery.com/jquery-3.3.1.min.js'
-
-const ACCESS_TOKEN = '32b920aecb767db1db87d2c7c010c9dd171cb9e56e7fa34391f47d97f25cb57c280ff53e9c2f4b940df87'
+import { ACCESS_TOKEN } from './credentials'
 
 function getPosts(groupId, groupName) {
     const keywords = [
-        'российские энергоносители',
-        'европа газ',
-        'европа нефть',
-        'европа уголь',
+        'паспорт болельщика',
+        'fan id',
     ]
-    const newsObj = []
+    const newsObj = [];
+    const dateRange = [];
     keywords.forEach((query) => {
         $.getJSON({
             url: `https://api.vk.com/method/wall.search?access_token=${ACCESS_TOKEN}&owner_id=-${groupId}&owners_only=1&query=${query}&count=100&v=5.131`,
@@ -20,22 +18,31 @@ function getPosts(groupId, groupName) {
                 const newsFields = {}
                 newsFields.date = new Date(
                     +news.date * 1000
-                ).toLocaleDateString('ru-RU')
-                newsFields.text = news.text
+                ).toLocaleDateString('ru-RU');
+                newsFields.text = news.text;
                 newsFields.url =
-                    'https://vk.com/wall-' + groupId + '_' + news.id
+                    'https://vk.com/wall-' + groupId + '_' + news.id;
                 if (
-                    newsFields.date.toString().includes('05.2022') ||
-                    newsFields.date.toString().includes('04.2022') ||
-                    newsFields.date.toString().includes('03.2022')
+                    newsFields.date.toString().includes('07.2022') ||
+                    newsFields.date.toString().includes('08.2022') ||
+                    newsFields.date.toString().includes('09.2022') ||
+                    newsFields.date.toString().includes('10.2022') ||
+                    newsFields.date.toString().includes('11.2022') ||
+                    newsFields.date.toString().includes('12.2022') ||
+                    newsFields.date.toString().includes('01.2023') ||
+                    newsFields.date.toString().includes('02.2023')
                 ) {
+                    dateRange.push(newsFields.date.substr(-7))
                     newsObj.push(newsFields)
                 }
+
             })
         })
-    })
+    },
+        )
 
     setTimeout(() => {
+        newsObj.push({frequency: calcMaxDateRange(dateRange)})
         download(groupName + '.json', JSON.stringify(Object.assign({}, newsObj)))
     }, 5000)
 }
@@ -56,24 +63,26 @@ function download(filename, text) {
     document.body.removeChild(element)
 }
 
+function calcMaxDateRange(news) {
+    if (!news) {return}
+    return news.sort((a,b) => 
+        news.filter(v => v===a).length - news.filter(v => v===b).length
+    ).pop();
+}
+
 function main() {
     const groups = [
-        {'tass': '26284064'},
-        {'russia 24': '62623421'},
-        {'vesti': '24136539'},
-        {'ria news': '15755094'},
         {'izvestiya': '27532693'},
         {'vedomosti': '15548215'},
         {'rbk': '25232578'},
         {'kommersant': '23482909'},
-        {'antiMaydan': '41232698'},
-        {'kp': '15722194'},
-        {'lentach': '29534144'}
+        {'Футбол | Чемпионат Мира 2022': '28639294'},
+        {'Российский Футбол: РПЛ': '25505197'},
     ]
     let i = 0;
 
     groups.forEach(group => {
-        i += 2
+        i += 5
         setTimeout(() => {
             getPosts(group[Object.keys(group)], Object.keys(group))
         }, i * 1000);
